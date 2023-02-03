07dfbb93a54adb9dd32f63e5fcbe1f51d6573d37
Md. Mehedy Hasan Tanvir <52083575+Mehedy-Tanvir@users.noreply.github.com>
2023-02-03 21:41:16 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const body = document.querySelector('body');
+body.addEventListener('mousemove', (event) => {
+    const xPos = event.offsetX;
+    const yPos = event.offsetY;
+    const span = document.createElement('span');
+    span.style.left = xPos + 'px';
+    span.style.top = yPos + 'px';
+    const size = Math.floor(Math.random() * 100) + 1;
+    span.style.width = size + 'px';
+    span.style.height = size + 'px';
+    body.appendChild(span);
+    setTimeout(() => {
+        span.remove();
+    }, 1000);
+})
